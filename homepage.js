d154bd87f580177d3ab39023c94a4ba0669aab75
Udhbhav Gupta <udhbhav25@gmail.com>
2017-10-27 22:45:57 -0500

    Add files via upload

--- homepage.js
+++ homepage.js
@@ -1,18 +1,29 @@
-/* attach a submit handler to the form */
+$('input').keydown(function (e) {
+    if (e.keyCode == 13) {
+        e.preventDefault();
+		var x = document.getElementById("formid");
+		var data = {data: x.elements[0].value};
+		var template = $("#form-template").html();
+		var html = Mustache.render(template, data);
+		$("#formid").append(html);
+		$('#date_field').focus();
+        return false;
+    }
+});
+
 $("#formid").submit(function(event) {
 
-  /* stop form from submitting normally */
-  event.preventDefault();
+	// stop form from submitting normally
+	event.preventDefault();
+	
+	// get the action attribute from the <form action=""> element 
+	var $form = $( this ),
+		url = $form.attr( 'action' );
+	// Send the data using post with element id name and name2
+	var posting = $.post( url, { name: $('#name_field').val(), date: $('#date_field').val() } );
 
-  /* get the action attribute from the <form action=""> element */
-  var $form = $( this ),
-      url = $form.attr( 'action' );
-  /* Send the data using post with element id name and name2*/
-  var posting = $.post( url, { name: $('#namespace').val() } );
-
-  /* Alerts the results */
-  posting.done(function( data ) {
-	var added = "<h3> When is " + data + "'s birthday?"
-   document.getElementById("innerForm").innerHTML = added;
-  });
+	// Alerts the results
+	posting.done(function( data ) {
+		$('.form').html("<h1>Success!</h1>");
+	});
 });
